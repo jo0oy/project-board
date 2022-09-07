f4c1a900e9680d88948c95ae4973863c6e9d2e2f
jo0oy <rkfkrh07@naver.com>
2022-09-07 13:37:56 +0900

    #2-domain class 설계 (게시글, 댓글)
    도메인 정보 설계.
    기초적인 게시글과 댓글 표현인 가능하도록 뼈대 설계.
    DB 와 연동을 위한 표현은 추후 업데이트 필요.

---
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -0,0 +1,8 @@
+package com.example.projectboard.domain;
+
+public class Article {
+    private Long id;
+    private String title; // 제목
+    private String content; // 본문
+    private String hashtag; // 해시태그
+}
