f90c46b71eb89d71e532b1bd515fd08698bedd8b
jo0oy <rkfkrh07@naver.com>
2022-09-08 08:54:42 +0900

    #3 - 도메인 클래스 패키지 구조로 변경
    도메인 클래스를 도메인별 패키지를 생성해 위치 변경시켰다.

--- src/main/java/com/example/projectboard/domain/Article.java
+++
@@ -1,8 +0,0 @@
-package com.example.projectboard.domain;
-
-public class Article {
-    private Long id;
-    private String title; // 제목
-    private String content; // 본문
-    private String hashtag; // 해시태그
-}
--- src/main/java/com/example/projectboard/domain/ArticleComment.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectboard.domain;
-
-public class ArticleComment {
-    private Long id;
-    private Article article; // 게시글 (id)
-    private String content; // 본문
-}
