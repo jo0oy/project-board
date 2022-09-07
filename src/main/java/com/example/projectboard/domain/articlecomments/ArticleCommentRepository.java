85688045e2a5b5fe3eb32001abf75b7842c1c13c
jo0oy <rkfkrh07@naver.com>
2022-09-08 08:53:27 +0900

    #3 - Repository Interface 작성
    repository 를 통해 DB 접근한다.

---
+++ src/main/java/com/example/projectboard/domain/articlecomments/ArticleCommentRepository.java
@@ -0,0 +1,13 @@
+package com.example.projectboard.domain.articlecomments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
+    @Query("select ac from ArticleComment ac where ac.article.id = :articleId")
+    List<ArticleComment> findByArticleId(@Param("articleId") Long articleId);
+}
---
+++ src/main/java/com/example/projectboard/domain/articles/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.example.projectboard.domain.articles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
+
