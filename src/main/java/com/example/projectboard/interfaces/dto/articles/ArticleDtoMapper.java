c06d396873da47e49c992ffe9a5471edb64e3a00
jo0oy <rkfkrh07@naver.com>
2022-09-19 14:11:19 +0900

    #5 - Article DTO & Mapper
    Article 도메인 DTO 와 MapStruct를 사용해 Mapper를 구현함

---
+++ src/main/java/com/example/projectboard/interfaces/dto/articles/ArticleDtoMapper.java
@@ -0,0 +1,25 @@
+package com.example.projectboard.interfaces.dto.articles;
+
+import com.example.projectboard.domain.articles.ArticleCommand;
+import com.example.projectboard.domain.articles.ArticleInfo;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public interface ArticleDtoMapper {
+
+    // DTO -> COMMAND
+    ArticleCommand.RegisterReq toCommand(ArticleDto.RegisterReq req);
+
+    ArticleCommand.UpdateReq toCommand(ArticleDto.UpdateReq req);
+
+    ArticleCommand.SearchCondition toCommand(ArticleDto.SearchCondition req);
+
+    // INFO -> DTO
+    ArticleDto.MainInfoResponse toDto(ArticleInfo info);
+}
