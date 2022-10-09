73f6497a4681d82c1eaf4bf8c48922aee9b47e17
jo0oy <rkfkrh07@naver.com>
2022-10-10 05:46:29 +0900

    #10 - 인증 로직 추가한 CommandService Test 수정
    * 인증 로직에 따라 정상 작동하는지, 권한 체크에 따라 올바른 Exception을 던지는지 Test 수정 및 추가 작성.
    * Spring Security Test Annotation 사용.
    * 인증이 필요한 테스트 맨 위 상단에 추가 : @WithUserDetails(value = "jo0oy", setupBefore = TestExecutionEvent.TEST_EXECUTION)

--- src/test/java/com/example/projectboard/application/articlecomments/ArticleCommentCommandServiceTest.java
+++ src/test/java/com/example/projectboard/application/articlecomments/ArticleCommentCommandServiceTest.java
@@ -1,11 +1,16 @@
 package com.example.projectboard.application.articlecomments;
 
+import com.example.projectboard.common.exception.NoAuthorityToUpdateDeleteException;
+import com.example.projectboard.common.exception.UsernameNotFoundException;
 import com.example.projectboard.domain.articlecomments.ArticleCommentCommand;
 import com.example.projectboard.domain.articlecomments.ArticleCommentRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 
 import javax.persistence.EntityManager;
 
@@ -21,12 +26,14 @@ class ArticleCommentCommandServiceTest {
     @Autowired
     private ArticleCommentCommandService sut;
 
+    @WithUserDetails(value = "jo0oy", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @DisplayName("[성공][service] 게시글 댓글 등록 테스트")
     @Test
     void givenRegisterReq_WhenRegisterComment_WorksFine() {
         // given
         var articleId = 3L;
         var content = "새로운 댓글 등록 테스트입니다.";
+        var username = "jo0oy";
         var req = ArticleCommentCommand.RegisterReq
                 .builder()
                 .articleId(articleId)
@@ -34,18 +41,38 @@ class ArticleCommentCommandServiceTest {
                 .build();
 
         // when
-        var result = sut.registerComment(req);
+        var result = sut.registerComment(username, req);
 
         // then
         assertThat(result.getArticleId()).isEqualTo(articleId);
         assertThat(result.getContent()).isEqualTo(content);
     }
 
-    @DisplayName("[성공][service] 게시글 댓글 수정 테스트")
+    @DisplayName("[실패][service] 게시글 댓글 등록 테스트 - 존재하지 않는 유저")
     @Test
-    void givenUpdateReq_WhenUpdate_WorksFine() {
+    void givenRegisterReqWithNotRegisteredUsername_WhenRegisterComment_ThenThrowsException() {
         // given
-        var updateCommentId = 5L;
+        var articleId = 3L;
+        var content = "새로운 댓글 등록 테스트입니다.";
+        var username = "noneUser";
+        var req = ArticleCommentCommand.RegisterReq
+                .builder()
+                .articleId(articleId)
+                .content(content)
+                .build();
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> sut.registerComment(username, req))
+                .isInstanceOf(UsernameNotFoundException.class)
+                .hasMessage("유저를 찾을 수 없습니다. username=" + username);
+    }
+
+    @DisplayName("[성공][service] 게시글 댓글 수정 테스트 - 수정 권한 있는 사용자")
+    @Test
+    void givenUpdateReqWithAuthorizedUsername_WhenUpdate_WorksFine() {
+        // given
+        var updateCommentId = 1L;
+        var username = "yooj";
         var updateContent = "업데이트할 댓글 내용입니다.";
         var req = ArticleCommentCommand.UpdateReq
                 .builder()
@@ -53,12 +80,59 @@ class ArticleCommentCommandServiceTest {
                 .build();
 
         // when
-        sut.update(updateCommentId, req);
+        sut.update(updateCommentId, username, req);
         em.clear();
 
         // then
         var updatedComment = articleCommentRepository.findById(updateCommentId).orElse(null);
         assertThat(updatedComment).isNotNull();
         assertThat(updatedComment.getContent()).isEqualTo(updateContent);
+    }
+
+    @DisplayName("[실패][service] 게시글 댓글 수정 테스트 - 수정 권한 없는 사용자")
+    @Test
+    void givenUpdateReqWithNotAuthorizedUsername_WhenUpdate_ThenThrowsException() {
+        // given
+        var updateCommentId = 4L;
+        var username = "yooj";
+        var updateContent = "업데이트할 댓글 내용입니다.";
+        var req = ArticleCommentCommand.UpdateReq
+                .builder()
+                .content(updateContent)
+                .build();
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> sut.update(updateCommentId, username, req))
+                .isInstanceOf(NoAuthorityToUpdateDeleteException.class)
+                .hasMessage("수정/삭제 권한이 없는 사용자입니다.");
+    }
+
+    @DisplayName("[실패][service] 게시글 댓글 삭제 테스트 - 삭제 권한 없는 사용자")
+    @Test
+    void givenCommentIdWithNotAuthorizedUsername_WhenDelete_ThenThrowsException() {
+        // given
+        var commentId = 4L;
+        var username = "yooj";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> sut.delete(commentId, username))
+                .isInstanceOf(NoAuthorityToUpdateDeleteException.class)
+                .hasMessage("수정/삭제 권한이 없는 사용자입니다.");
+    }
+
+    @DisplayName("[성공][service] 게시글 댓글 삭제 테스트 - 삭제 권한 있는 사용자")
+    @Test
+    void givenCommentIdWithAuthorizedUsername_WhenDelete_WorksFine() {
+        // given
+        var commentId = 2L;
+        var username = "jo0oy";
+        var beforeDeleteTotal = articleCommentRepository.count();
+
+        // when
+        sut.delete(commentId, username);
+        em.clear();
+
+        // then
+        assertThat(articleCommentRepository.count()).isEqualTo(beforeDeleteTotal - 1);
     }
 }
--- src/test/java/com/example/projectboard/application/articles/ArticleCommandServiceTest.java
+++ src/test/java/com/example/projectboard/application/articles/ArticleCommandServiceTest.java
@@ -1,17 +1,25 @@
 package com.example.projectboard.application.articles;
 
+import com.example.projectboard.common.exception.NoAuthorityToUpdateDeleteException;
+import com.example.projectboard.common.exception.UsernameNotFoundException;
+import com.example.projectboard.config.JpaConfig;
 import com.example.projectboard.domain.articlecomments.ArticleCommentRepository;
 import com.example.projectboard.domain.articles.ArticleCommand;
 import com.example.projectboard.domain.articles.ArticleRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 
 import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Import(JpaConfig.class)
 @SpringBootTest
 class ArticleCommandServiceTest {
 
@@ -25,6 +33,7 @@ class ArticleCommandServiceTest {
     @Autowired
     private ArticleCommentRepository articleCommentRepository;
 
+    @WithUserDetails(value = "jo0oy", setupBefore = TestExecutionEvent.TEST_EXECUTION)
     @DisplayName("[성공][service] 게시물 등록 테스트")
     @Test
     void givenRegisterReq_whenRegisterArticle_thenWorksFine() {
@@ -32,6 +41,7 @@ class ArticleCommandServiceTest {
         var title = "새 게시글 제목";
         var content = "새 게시글 내용입니다!!";
         var hashtag = "#newPost";
+        var username = "jo0oy";
         var req = ArticleCommand.RegisterReq.builder()
                 .title(title)
                 .content(content)
@@ -39,7 +49,7 @@ class ArticleCommandServiceTest {
                 .build();
 
         // when
-        var result = sut.registerArticle(req);
+        var result = sut.registerArticle(username, req);
 
         // then
         assertThat(result.getTitle()).isEqualTo(title);
@@ -47,11 +57,32 @@ class ArticleCommandServiceTest {
         assertThat(result.getHashtag()).isEqualTo(hashtag);
     }
 
-    @DisplayName("[성공][service] 게시물 수정 테스트")
+    @DisplayName("[실패][service] 게시물 등록 테스트 - 존재하지 않는 사용자")
     @Test
-    void givenUpdateReq_whenUpdate_thenArticleUpdated() {
+    void givenRegisterReqWithNotRegisteredUsername_whenRegisterArticle_thenThrowsException() {
         // given
-        var articleId = 5L;
+        var title = "새 게시글 제목";
+        var content = "새 게시글 내용입니다!!";
+        var hashtag = "#newPost";
+        var username = "noneUser";
+        var req = ArticleCommand.RegisterReq.builder()
+                .title(title)
+                .content(content)
+                .hashtag(hashtag)
+                .build();
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> sut.registerArticle(username, req))
+                .isInstanceOf(UsernameNotFoundException.class)
+                .hasMessage("유저를 찾을 수 없습니다. username=" + username);
+    }
+
+    @DisplayName("[성공][service] 게시물 수정 테스트 - 수정 권한 있는 사용자")
+    @Test
+    void givenUpdateReqWithAuthorizedUsername_whenUpdate_thenArticleUpdated() {
+        // given
+        var articleId = 4L;
+        var username = "jo0oy";
         var updateTitle = "수정할 게시글 제목";
         var updateContent = "수정할 게시글 내용입니다.";
         var req = ArticleCommand.UpdateReq
@@ -61,7 +92,7 @@ class ArticleCommandServiceTest {
                 .build();
 
         // when
-        sut.update(articleId, req);
+        sut.update(articleId, username, req);
         em.clear();
 
         // then
@@ -73,15 +104,36 @@ class ArticleCommandServiceTest {
         assertThat(findArticle.getContent()).isEqualTo(updateContent);
     }
 
-    @DisplayName("[성공][service] 게시물 삭제 테스트")
+    @DisplayName("[실패][service] 게시물 수정 테스트 - 수정 권한이 없는 사용자")
     @Test
-    void givenArticleId_whenDelete_thenDeleteCommentsAndArticle() {
+    void givenUpdateReqWithNoneAuthorizedUsername_whenUpdate_thenThrowsException() {
+        // given
+        var articleId = 4L;
+        var username = "yooj";
+        var updateTitle = "수정할 게시글 제목";
+        var updateContent = "수정할 게시글 내용입니다.";
+        var req = ArticleCommand.UpdateReq
+                .builder()
+                .title(updateTitle)
+                .content(updateContent)
+                .build();
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> sut.update(articleId, username, req))
+                .isInstanceOf(NoAuthorityToUpdateDeleteException.class)
+                .hasMessage("수정/삭제 권한이 없는 사용자입니다.");
+    }
+
+    @DisplayName("[성공][service] 게시물 삭제 테스트 - 삭제 권한 있는 사용자")
+    @Test
+    void givenArticleIdWithAuthorizedUsername_whenDelete_thenDeleteCommentsAndArticle() {
         // given
         var articleId = 1L;
+        var username = "yooj";
         var beforeDeleteCommentListSize = articleCommentRepository.findByArticleId(articleId).size();
 
         // when
-        sut.delete(articleId);
+        sut.delete(articleId, username);
 
         // then
         System.out.println("==================AFTER WHEN EXECUTED==================");
@@ -91,5 +143,18 @@ class ArticleCommandServiceTest {
         assertThat(findArticle).isNotPresent();
         assertThat(comments.size()).isEqualTo(0);
         assertThat(beforeDeleteCommentListSize).isNotEqualTo(comments.size());
+    }
+
+    @DisplayName("[실패][service] 게시물 삭제 테스트 - 삭제 권한 없는 사용자")
+    @Test
+    void givenArticleIdWithNoneAuthorizedUsername_whenDelete_thenThrowsException() {
+        // given
+        var articleId = 1L;
+        var username = "jo0oy";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> sut.delete(articleId, username))
+                .isInstanceOf(NoAuthorityToUpdateDeleteException.class)
+                .hasMessage("수정/삭제 권한이 없는 사용자입니다.");
     }
 }
