664bce59d5044811c5385e4d999b0b063b6b4faf
jo0oy <rkfkrh07@naver.com>
2022-12-10 09:42:27 +0900

    #23 - 대댓글 기능을 위한 ArticleComment 도메인 변경
    * ArticleComment 도메인에 부모 댓글/ 자식 댓글 리스트 연관관계 수정 업데이트함. 
    * ArticleComment 생성 메서드 빌더 --> static 메소드로 변경함.
    * 위의 변경에 따라 ArticleCommentCommand의 toEntity() 메서드 변경함.
    * parent 댓글이 삭제 되었을 때 자식 댓글들 모두 삭제될 수 있도록 벌크 삭제 메서드 작성함.

--- src/main/java/com/example/projectboard/domain/articlecomments/ArticleComment.java
+++ src/main/java/com/example/projectboard/domain/articlecomments/ArticleComment.java
@@ -6,7 +6,9 @@ import lombok.*;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -18,7 +20,8 @@ import java.util.Objects;
 @Entity
 public class ArticleComment extends JpaAuditingFields {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "article_comment_id")
     private Long id;
 
@@ -31,17 +34,52 @@ public class ArticleComment extends JpaAuditingFields {
     @Column(nullable = false, updatable = false)
     private Long userId; // 연관관계 매핑: UserAccount id
 
-    @Builder
-    private ArticleComment(Article article,
-                           String commentBody,
-                           Long userId) {
-        this.article = article;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", updatable = false)
+    private ArticleComment parent;
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC") // 대댓글은 최신순으로 정렬한다.
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childs = new LinkedHashSet<>();
+
+    private ArticleComment(String commentBody,
+                           Article article,
+                           Long userId,
+                           ArticleComment parent) {
         this.commentBody = commentBody;
+        this.article = article;
         this.userId = userId;
+        this.parent = parent;
+    }
+
+    public static ArticleComment of(String commentBody,
+                                    Article article,
+                                    Long userId) {
+
+        return new ArticleComment(commentBody, article, userId, null);
+    }
+
+    public static ArticleComment of(String commentBody,
+                                    Article article,
+                                    Long userId,
+                                    ArticleComment parent) {
+
+        return new ArticleComment(commentBody, article, userId, parent);
+    }
+
+    public void setParent(ArticleComment parent) {
+        this.parent = parent;
+    }
+
+    // 부모-자식 양방향 매핑 메서드
+    public void addChild(ArticleComment child) {
+        child.setParent(this);
+        this.getChilds().add(child);
     }
 
     public void update(String content) {
-        if(StringUtils.hasText(content)) this.commentBody = content;
+        if (StringUtils.hasText(content)) this.commentBody = content;
     }
 
     // equals & hashcode override
@@ -58,4 +96,3 @@ public class ArticleComment extends JpaAuditingFields {
         return Objects.hash(id);
     }
 }
-
